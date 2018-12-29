9a932c6e8e136997a86d46b25574cc3d948ab67e
Ryan Nevius <ryanevius@gmail.com>
2018-12-29 18:08:02 +0700

    Add episodes 01 and 02
    01 – refactoring tests from var to this
    02 – using the expression register to calculate ages

---
+++ 01_from_var_to_this.js
@@ -0,0 +1,15 @@
+(function (namespace) {
+  buster.testCase("test", {
+
+    setup: function() {
+      var controller = {};
+      var panel = panel.creat();
+      var listener = this.spy();
+
+    },
+
+    "test should do stuff": function() {
+      // ...
+    },
+  });
+}());
---
+++ intro.md
@@ -0,0 +1,7 @@
+
+
+
+        @vimrocks :: episode xx
+
+
+
